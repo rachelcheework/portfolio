b426c5a527651c56e0c0c863982441ccf35d80e1
rachelcheework <rachelcheework@gmail.com>
2024-01-03 18:41:00 +0800

    testing

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,7 @@ import React from "react";
 const Homepage=()=>{
     return (
 <div>
-    <div class="container-fluid" id="content">
+    {/* <div class="container-fluid" id="content">
         <div class="row">
             <div class="col-lg-8">
                 <h1>Hi, I'm Rachel!</h1>
@@ -14,7 +14,12 @@ const Homepage=()=>{
                 <img src="../images/me.jpeg" alt="picture of me"/>
             </div>
         </div>
-    </div>
+    </div> */}
+
+    <h1>Hi, I'm Rachel!</h1>
+    <p>Welcome to my website - I hope you find something interesting here!</p>
+    <img src="../images/me.jpeg" alt="picture of me"/>
+
 </div>
     );
 }
